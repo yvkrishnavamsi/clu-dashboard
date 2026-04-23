bd6a2425f0b840e04c05d771884d39e1660a8c2b
yvkrishnavamsi <yvkrishnavamsi@gmail.com>
2026-04-23 23:51:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 st.set_page_config(
     page_title="CLU Applications Dashboard",
     layout="wide",
-    page_icon="📊",
+    page_icon="aplogo.png",
     initial_sidebar_state="expanded"
 )
 
